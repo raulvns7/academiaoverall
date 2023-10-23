26aef0a3fb27cd8aff12f6990ed439d7bcb0e66a
Raul Felipe Rodrigues <94416198+raulvns7@users.noreply.github.com>
2023-10-23 10:29:54 -0300

    Add files via upload

---
+++ AcademiaOverall/src/CadastroAlunos/Cadastro.java
@@ -0,0 +1,50 @@
+package CadastroAlunos;
+
+public class Cadastro {
+
+    private String nome;
+    private String sobrenome;
+
+    private int idade;
+
+    private String endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+    public String getEndereco() {
+
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+
+        this.endereco = endereco;
+    }
+
+}
+
+
+
---
+++ AcademiaOverall/src/Main.java
@@ -0,0 +1,18 @@
+import CadastroAlunos.Cadastro;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Cadastro aluno1 = new Cadastro();
+        aluno1.setNome("Raul Felipe");
+        System.out.println("NOME DO ALUNO: " + aluno1.getNome());
+
+
+
+
+
+
+    }
+
+}
